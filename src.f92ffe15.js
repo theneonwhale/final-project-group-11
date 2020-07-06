0aa6e901cd2bbba384decb526485b15259bcab9e
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-07-06 22:36:13 +0300

    Updates

--- src.f92ffe15.js
+++ src.f92ffe15.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.884e08e2.js.map
+//# sourceMappingURL=%27/final-project-group-11/%27/src.f92ffe15.js.map
