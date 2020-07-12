9d7d8c036b9b13c4d7f2d64f3d9a181f7b2618f2
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-07-12 13:42:22 +0300

    Updates

--- modal.130e4d6b.js
+++ modal.130e4d6b.js
@@ -120,12 +120,16 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"modal.js":[function(require,module,exports) {
 (function () {
   var refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
     modal: document.querySelector('[data-modal]')
   };
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(function (el) {
+    el.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.forEach(function (el) {
+    el.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
@@ -160,7 +164,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49778" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57767" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
